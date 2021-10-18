4f79e3810503f2f8b6e4a8c29d4fe3b75bb9a387
Chris K <cgkocur@gmail.com>
2021-10-18 12:23:46 -0400

    working on a refreshing token

--- chatbot/chatbot.js
+++ chatbot/chatbot.js
@@ -1,21 +1,31 @@
 require('dotenv').config()
-const { StaticAuthProvider } = require('@twurple/auth')
+const { promises: fs } = require('fs')
+const { RefreshingAuthProvider } = require('@twurple/auth')
 const { ChatClient } = require('@twurple/chat')
+
 
 console.log(process.env.LOAD_SUCCESS)
 
 async function main(){
+    const tokenData = JSON.parse(await fs.readFile('./token.json'))
     const clientId = process.env.BOT_CLIENT_ID
     const accessToken = process.env.BOT_PASSWORD
     const BOT_SECRET = process.env.BOT_SECRET
     const MY_CHANNEL = 'totescoax'
-    const authProvider = new StaticAuthProvider(clientId, accessToken)
+    const authProvider = new RefreshingAuthProvider(
+        {
+            clientId,
+            BOT_SECRET,
+            onRefresh: async newTokenData => await fs.writeFile('./token.json', JSON.stringify(newTokenData, null, 4), 'utf-8')
+        },
+        tokenData
+    )
     const CHAT = new ChatClient({ authProvider, channels: [MY_CHANNEL] })
     await CHAT.connect().catch(console.error())
 
     CHAT.onMessage((channel, user, message, msg) => {
         console.log(`${user}: ${message}`)
-        console.log(msg.userInfo.color)
+        //console.log(msg.userInfo.color)
     })
 
 }
