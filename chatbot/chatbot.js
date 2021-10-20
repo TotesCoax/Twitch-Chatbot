d4e1842baf1ba249a912d77f845a9735b083cb60
Chris K <cgkocur@gmail.com>
2021-10-20 13:48:37 -0400

    testing reponses fromt heb ot

--- chatbot/chatbot.js
+++ chatbot/chatbot.js
@@ -10,7 +10,6 @@ console.log(process.env.LOAD_SUCCESS)
 
 async function main(){
     const clientId = process.env.BOT_CLIENT_ID
-    const accessToken = process.env.BOT_PASSWORD
     const tokenData = JSON.parse(await fs.readFile('./token.json'))
     const clientSecret = process.env.BOT_SECRET
     const MY_CHANNEL = 'totescoax'
@@ -22,20 +21,24 @@ async function main(){
         },
         tokenData
         )
-    //Need to update the token permissions for this to officially work.
+    //PubSub connection for channel points redemptions
     const pubSubClient = new PubSubClient()
     const userId = await pubSubClient.registerUserListener(authProvider)
-    console.log(userId)
+        .then(console.log('Connected to PubSub'))
     pubSubClient.onRedemption(userId, (message) => {
         console.log(message.rewardTitle);
     })
+
+    //Connection to chat
     const CHAT = new ChatClient({ authProvider, channels: [MY_CHANNEL] })
     await CHAT.connect().catch(console.error())
-        .then(console.log('Bot connected!'))
+        .then(console.log('Connected to chat'))
 
     CHAT.onMessage((channel, user, message, msg) => {
         console.log(`${user}: ${message}`)
-        //console.log(msg.userInfo.color)
+        if (message === "test"){
+            CHAT.say(channel, 'The bot acknowledges your test.')
+        }
     })
 
 }
